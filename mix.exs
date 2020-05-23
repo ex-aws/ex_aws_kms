17c6f9a259111f1f2ec96d7768c8fa574b955e0f
Art Kay <art@artkay.net>
2020-05-23 13:51:51 -0400

    Fix code formatting issues

--- mix.exs
+++ mix.exs
@@ -6,14 +6,14 @@ defmodule ExAws.KMS.Mixfile do
       app: :ex_aws_kms,
       version: "2.1.0",
       elixir: "~> 1.5",
-      elixirc_paths: elixirc_paths(Mix.env),
-      start_permanent: Mix.env == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib",]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help compile.app" to learn about applications.
   def application do
@@ -27,7 +27,7 @@ defmodule ExAws.KMS.Mixfile do
     [
       {:hackney, ">= 0.0.0", only: [:dev, :test]},
       {:poison, ">= 0.0.0", only: [:dev, :test]},
-      ex_aws(),
+      ex_aws()
     ]
   end
 
